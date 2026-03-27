a36f99b65b678c37da8bb963c4ad4e1f9e5756ba
Giwrgos Pouliakis <georgepouliakis02@gmail.com>
2026-03-27 11:47:40 +0200

    love calculator
    ενα πολυ μικρο προγραμμα που στο onclick σου εμφανιζει prompt και μετα σου δειχνει ενα σκορ

---
+++ Love calculator/love.js
@@ -0,0 +1,8 @@
+function loveCalculator(){
+    var name1 = prompt("what's your name?");
+    var name2 = prompt("What's your crush name?");
+
+    var loveScore = Math.floor((Math.random()*100)+1);
+
+    alert("Το σκορ αγάπης σας είναι " + loveScore + "%");
+}
