bdc5c45a9b284885c24e5ddfe29a754f061cfee0
Giwrgos Pouliakis <georgepouliakis02@gmail.com>
2026-05-29 14:03:01 +0300

    Add files via upload

---
+++ Random Quote Generator/index.js
@@ -0,0 +1,25 @@
+const quotes = [
+    "Hello this is my life",
+    "What's up buddy",
+    "How is your day today",
+    "Your Favourite hobby"
+]
+
+const usedIndexes = new Set();
+const quoteElement = document.getElementById("quote");
+
+function generateQuote() {
+    if (usedIndexes.size >= quotes.length) {
+        usedIndexes.clear();
+    }
+    while(true){
+        const randomIdx = Math.floor(Math.random() * quotes.length);
+
+        if (usedIndexes.has(randomIdx)) continue;
+
+        const quote = quotes[randomIdx];
+        quoteElement.innerHTML = quote;
+        usedIndexes.add(randomIdx);
+        break;
+    }
+}
